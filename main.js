babdb101ec2e4a92baea2c0256b3125a49f8e745
fahadbinsiddique <fahad.bin.siddique345@gmail.com>
2024-09-15 02:11:25 +0600

    js 1

---
+++ main.js
@@ -0,0 +1,111 @@
+//  Ans to the Question No.1 
+        // Snake Case
+
+let sarahasapple = 25;
+let sarabuyfromshop = 15;
+
+let sarahastotalapple = sarahasapple + sarabuyfromshop;
+
+let sarafinalresult = '1.'+'Sara' + ' '+ 'Has'+ ' '+ 'Total' +' '+'Apple' +' ' + sarahastotalapple + '.';
+
+console.log(sarafinalresult);
+
+
+//  Ans to the Question No.2
+        //Pascal Case
+
+let JohnHas = 5000;
+let JohnSpent = 2000;
+
+let remainingmoney = JohnHas - JohnSpent
+
+let johnfinalresult = '2.'+'Now'+' '+'Jhon'+' '+'Has'+ ' ' + remainingmoney + ' '
++'TK'+' '+'Left.'
+
+console.log(johnfinalresult)
+
+//  Ans to the Question No.3
+        // Camel Case
+
+let perHourMachine = 120;
+let howMachineCreate = 8;
+
+let totalMachineCreate = perHourMachine * howMachineCreate
+
+let machineFinalResult = '3.' + totalMachineCreate +  ' ' +'Machines'+' '+'Will' + ' ' + 'Be'+ ' '+ 'Produced'+ ' '+ 'In'+ ' '+ '8'+' '+'Hours.'
+
+console.log(machineFinalResult)
+
+//  Ans to the Question No.4
+        //Kebab Case
+
+let TREE_PER_ROW = 12;
+let ROW = 8;
+let ADD_MORE_TREE = 20;
+
+let TOTAL_TREE = TREE_PER_ROW * ROW + ADD_MORE_TREE
+
+let TREE_FINAL_RESULT = '4.' + 'There' + ' '+ 'Are'+ ' ' + TOTAL_TREE +  ' '+ 'Trees'+ ' '+ 'In'+ ' '+'The' + ' '+'Garden.'
+
+console.log(TREE_FINAL_RESULT)
+
+//  Ans to the Question No.5
+        // snake case
+
+let rahimhasbox= 5;
+let perboxhasorange= 10;
+let rahimgifthisfriend = 12;
+
+let rahimhaveorange = rahimhasbox * perboxhasorange - rahimgifthisfriend 
+
+let rahimfinalresult = "5." +'Rahim' + ' '+'Has'+ ' '+ rahimhaveorange + ' '+'Oranges.'
+
+console.log(rahimfinalresult)
+
+//  Ans to the Question No.6
+
+let traincross= 150;
+let traintakestime = 3;
+
+let crossinperhour = traincross / traintakestime
+
+let trainfinalresult = '6.'+ 'The'+ ' '+'Train'+ ' '+'Covers'+ ' '+ crossinperhour+ ' '+'KM'+ ' '+'Per'+ ' '+'Hour.'
+
+console.log(trainfinalresult)
+
+//  Ans to the Question No.7
+
+let mechine_in_factory = 6;
+let product_manufactured = 200;
+let add_mechine = 2;
+let unsold_product = 3; 
+
+let per_mechine_manufacture = Math.round (product_manufactured / mechine_in_factory)
+
+let two_mechine_add = per_mechine_manufacture * add_mechine
+
+let total_product_manufactured = product_manufactured + two_mechine_add
+
+let remaining_product = total_product_manufactured + unsold_product
+
+let product_final_result = '7.'+'Total Product Is'+ ' '+ total_product_manufactured + ' '+ 'But If 3 Unsold The Count Is'+ ' '+ remaining_product+'.'
+
+console.log(product_final_result)
+
+//  Ans to the Question No.8
+
+let choclate_buy = 120;
+let per_packet_capacity = 9;
+
+let choclate_packet = Math.round(choclate_buy / per_packet_capacity);
+
+let packed_choclate = per_packet_capacity * choclate_packet;
+
+let remaining_choclate = choclate_buy - packed_choclate
+
+let choclate_final_result = '8.'+'The Shopkeeper Will Have'+ ' '+remaining_choclate+ ' '+'Chocolates Left.'
+
+
+console.log(choclate_final_result)
+
+
